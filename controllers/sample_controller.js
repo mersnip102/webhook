fcf79bf6deb2b1a3b33d8d437ed363d9f5abcbd8
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-08-09 16:17:26 +0700

    first commit

--- controllers/sample_controller.js
+++ controllers/sample_controller.js
@@ -3,14 +3,14 @@ const util = require('./util');
 const handleSampleResponse = (req) => {
 
     console.log(req.body);
-
+    let targetFlow = 'projects/trusty-dialect-395403/locations/asia-southeast1/agents/816be4c6-373f-472b-b0f5-ac2c81f84e03/flows/41872baa-2e9a-4d06-92e0-7bf9317dcf29'
     return util.formatResponseForDialogflow(
         [
             'This is a sample response from webhook.',
             'Another sample response.'
         ],
         '',
-        '',
+        targetFlow,
         ''
     );
 };

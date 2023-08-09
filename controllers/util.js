9544a9b11c454a4ba6bf2a76b8a8536f15bde0d2
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-08-09 17:12:12 +0700

    first commit

--- controllers/util.js
+++ controllers/util.js
@@ -1,63 +1,37 @@
 const formatResponseForDialogflow = (texts, sessionInfo, targetFlow, targetPage) => {
 
-    // messages = []
+    messages = []
 
-    // texts.forEach(text => {
-    //     messages.push(
-    //         {
-    //             text: {
-    //                 text: [text],
-    //                 redactedText: [text]
-    //             },
-    //             responseType: 'HANDLER_PROMPT',
-    //             source: 'VIRTUAL_AGENT'
-    //         }
-    //     );
-    // });
+    texts.forEach(text => {
+        messages.push(
+            {
+                text: {
+                    text: [text],
+                    redactedText: [text]
+                },
+                responseType: 'HANDLER_PROMPT',
+                source: 'VIRTUAL_AGENT'
+            }
+        );
+    });
 
-    // let responseData = {
-    //     fulfillment_response: {
-    //         messages: messages
-    //     }
-    // };
+    let responseData = {
+        fulfillment_response: {
+            messages: messages
+        }
+    };
 
-    // if (sessionInfo !== '') {
-    //     responseData['sessionInfo'] = sessionInfo;
-    // }
+    if (sessionInfo !== '') {
+        responseData['sessionInfo'] = sessionInfo;
+    }
 
-    // if (targetFlow !== '') {
-    //     responseData['targetFlow'] = targetFlow;
-    // }
+    if (targetFlow !== '') {
+        responseData['targetFlow'] = targetFlow;
+    }
 
-    // if (targetPage !== '') {
-    //     responseData['targetPage'] = targetPage;
-    // }
-
-    const suggestion1 = {
-        title: 'Option 1',
-      };
-      const suggestion2 = {
-        title: 'Option 2',
-      };
-      const suggestion3 = {
-        title: 'Option 3',
-      };
-      
-      const suggestions = [suggestion1, suggestion2, suggestion3];
-      
-      const responseData = {
-        fulfillmentMessages: [
-          {
-            platform: 'PLATFORM_UNSPECIFIED',
-            suggestions: {
-              suggestions: suggestions,
-            },
-            text: {
-              text: ['Please select an option.'],
-            },
-          },
-        ],
-      };
+    if (targetPage !== '') {
+        responseData['targetPage'] = targetPage;
+    }
 
     return responseData
 };

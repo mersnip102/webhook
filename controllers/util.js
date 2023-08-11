dfa562aa01df9062508035e059deac543806f695
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-08-11 17:44:48 +0700

    first commit

--- controllers/util.js
+++ controllers/util.js
@@ -1,44 +1,79 @@
 const formatResponseForDialogflow = (texts, sessionInfo, targetFlow, targetPage) => {
 
     messages = []
-
-    texts.forEach(text => {
-        messages.push(
-            {
-                // text: {
-                //     text: [text],
-                //     redactedText: [text]
-                // },
-                payload: {
-                    "richContent": [
-                      [
-                        {
-                          "type": "list",
-                          "title": "List item 1 title",
-                          "subtitle": "List item 1 subtitle",
-                          "event": {
-                            "name": ""
-                          }
-                        },
-                        {
-                          "type": "divider"
-                        },
-                        {
-                          "type": "list",
-                          "title": "List item 2 title",
-                          "subtitle": "List item 2 subtitle",
-                          "event": {
-                            "name": ""
-                          }
-                        }
-                      ]
-                    ]
+    messages.push(
+      {
+          // text: {
+          //     text: [text],
+          //     redactedText: [text]
+          // },
+          payload: {
+              "richContent": [
+                [
+                  {
+                    "type": "list",
+                    "title": "List item 1 title",
+                    "subtitle": "List item 1 subtitle",
+                    "event": {
+                      "name": ""
+                    }
                   },
-                responseType: 'HANDLER_PROMPT',
-                source: 'VIRTUAL_AGENT'
-            }
-        );
-    });
+                  {
+                    "type": "divider"
+                  },
+                  {
+                    "type": "list",
+                    "title": "List item 2 title",
+                    "subtitle": "List item 2 subtitle",
+                    "event": {
+                      "name": ""
+                    }
+                  }
+                ]
+              ]
+            },
+          responseType: 'HANDLER_PROMPT',
+          source: 'VIRTUAL_AGENT'
+      }
+  );
+
+    // texts.forEach(text => {
+    //     messages.push(
+    //         {
+    //             // text: {
+    //             //     text: [text],
+    //             //     redactedText: [text]
+    //             // },
+    //             payload: {
+    //                 "richContent": [
+    //                   [
+    //                     {
+    //                       "type": "list",
+    //                       "title": "List item 1 title",
+    //                       "subtitle": "List item 1 subtitle",
+    //                       "event": {
+    //                         "name": ""
+    //                       }
+    //                     },
+    //                     {
+    //                       "type": "divider"
+    //                     },
+    //                     {
+    //                       "type": "list",
+    //                       "title": "List item 2 title",
+    //                       "subtitle": "List item 2 subtitle",
+    //                       "event": {
+    //                         "name": ""
+    //                       }
+    //                     }
+    //                   ]
+    //                 ]
+    //               },
+    //             responseType: 'HANDLER_PROMPT',
+    //             source: 'VIRTUAL_AGENT'
+    //         }
+    //     );
+    // });
 
     let responseData = {
         fulfillment_response: {

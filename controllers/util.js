ba74f5af49294bab8f023dfaa8f7ddd398b68ad8
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-08-11 18:13:03 +0700

    first commit

--- controllers/util.js
+++ controllers/util.js
@@ -1,129 +1,134 @@
 const formatResponseForDialogflow = (texts, sessionInfo, targetFlow, targetPage) => {
 
-    messages = []
-    messages.push(
-      {
-          // text: {
-          //     text: [texts[0]],
-          //     // redactedText: [text]
-          // },
-          payload: {
-              "richContent": [
-                [
-                  {
-                    "type": "list",
-                    "title": "List item 1 title",
-                    "subtitle": "List item 1 subtitle",
-                    "event": {
-                      "name": ""
-                    }
-                  },
-                  {
-                    "type": "divider"
-                  },
-                  {
-                    "type": "list",
-                    "title": "List item 2 title",
-                    "subtitle": "List item 2 subtitle",
-                    "event": {
-                      "name": ""
-                    }
-                  }
-                ]
-              ]
-            },
-          responseType: 'HANDLER_PROMPT',
-          source: 'VIRTUAL_AGENT'
-      }
+  messages = []
+  messages.push(
+    {
+      text: {
+        text: [texts],
+        redactedText: [texts[0]]
+      },
+
+      responseType: 'HANDLER_PROMPT',
+      source: 'VIRTUAL_AGENT'
+    }
   );
   messages.push(
     {
-        text: {
-            text: [texts],
-            redactedText: [texts[0]]
-        },
-       
-        responseType: 'HANDLER_PROMPT',
-        source: 'VIRTUAL_AGENT'
+      // text: {
+      //     text: [texts[0]],
+      //     // redactedText: [text]
+      // },
+      payload: {
+        "richContent": [
+          [
+            {
+              "type": "chips",
+              "options": [
+                {
+                  "text": "Chip 1",
+                  "image": {
+                    "src": {
+                      "rawUrl": "https://example.com/images/logo.png"
+                    }
+                  },
+                  "link": "https://example.com"
+                },
+                {
+                  "text": "Chip 2",
+                  "image": {
+                    "src": {
+                      "rawUrl": "https://example.com/images/logo.png"
+                    }
+                  },
+                  "link": "https://example.com"
+                }
+              ]
+            }
+          ]
+        ]
+      },
+      responseType: 'HANDLER_PROMPT',
+      source: 'VIRTUAL_AGENT'
     }
-);
+  );
 
-    // texts.forEach(text => {
-    //     messages.push(
-    //         {
-    //             // text: {
-    //             //     text: [text],
-    //             //     redactedText: [text]
-    //             // },
-    //             payload: {
-    //                 "richContent": [
-    //                   [
-    //                     {
-    //                       "type": "list",
-    //                       "title": "List item 1 title",
-    //                       "subtitle": "List item 1 subtitle",
-    //                       "event": {
-    //                         "name": ""
-    //                       }
-    //                     },
-    //                     {
-    //                       "type": "divider"
-    //                     },
-    //                     {
-    //                       "type": "list",
-    //                       "title": "List item 2 title",
-    //                       "subtitle": "List item 2 subtitle",
-    //                       "event": {
-    //                         "name": ""
-    //                       }
-    //                     }
-    //                   ]
-    //                 ]
-    //               },
-    //             responseType: 'HANDLER_PROMPT',
-    //             source: 'VIRTUAL_AGENT'
-    //         }
-    //     );
-    // });
 
-    let responseData = {
-        fulfillment_response: {
-            messages: messages
-        }
-    };
+  // texts.forEach(text => {
+  //     messages.push(
+  //         {
+  //             // text: {
+  //             //     text: [text],
+  //             //     redactedText: [text]
+  //             // },
+  //             payload: {
+  //                 "richContent": [
+  //                   [
+  //                     {
+  //                       "type": "list",
+  //                       "title": "List item 1 title",
+  //                       "subtitle": "List item 1 subtitle",
+  //                       "event": {
+  //                         "name": ""
+  //                       }
+  //                     },
+  //                     {
+  //                       "type": "divider"
+  //                     },
+  //                     {
+  //                       "type": "list",
+  //                       "title": "List item 2 title",
+  //                       "subtitle": "List item 2 subtitle",
+  //                       "event": {
+  //                         "name": ""
+  //                       }
+  //                     }
+  //                   ]
+  //                 ]
+  //               },
+  //             responseType: 'HANDLER_PROMPT',
+  //             source: 'VIRTUAL_AGENT'
+  //         }
+  //     );
+  // });
 
-    if (sessionInfo !== '') {
-        responseData['sessionInfo'] = sessionInfo;
+  let responseData = {
+    fulfillment_response: {
+      messages: messages
     }
+  };
 
-    if (targetFlow !== '') {
-        responseData['targetFlow'] = targetFlow;
-    }
+  if (sessionInfo !== '') {
+    responseData['sessionInfo'] = sessionInfo;
+  }
 
-    if (targetPage !== '') {
-        responseData['targetPage'] = targetPage;
-    }
+  if (targetFlow !== '') {
+    responseData['targetFlow'] = targetFlow;
+  }
 
-    
-        
+  if (targetPage !== '') {
+    responseData['targetPage'] = targetPage;
+  }
 
-    return responseData
+
+
+
+  return responseData
 };
 
 const getErrorMessage = () => {
 
-    return formatResponseForDialogflow(
-        [
-            'We are facing a technical issue.',
-            'Please try after sometimes or contact the XYZ restaurant.'
-        ],
-        '',
-        '',
-        ''
-    );
+  return formatResponseForDialogflow(
+    [
+      'We are facing a technical issue.',
+      'Please try after sometimes or contact the XYZ restaurant.'
+    ],
+    '',
+    '',
+    ''
+  );
 };
 
 module.exports = {
-    formatResponseForDialogflow,
-    getErrorMessage
+  formatResponseForDialogflow,
+  getErrorMessage
 };

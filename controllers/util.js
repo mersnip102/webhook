48592cd2ff758652007ac13bea17741fce85127c
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-08-11 17:05:34 +0700

    first commit

--- controllers/util.js
+++ controllers/util.js
@@ -9,50 +9,41 @@ const formatResponseForDialogflow = (texts, sessionInfo, targetFlow, targetPage)
                 text: [texts],
                 redactedText: [texts[0]]
             },
+            payload:{
+              "richContent": [
+                [
+                  {
+                    "type": "chips",
+                    "options": [
+                      {
+                        "text": "Chip 1",
+                        "image": {
+                          "src": {
+                            "rawUrl": "https://example.com/images/logo.png"
+                          }
+                        },
+                        "link": "https://example.com"
+                      },
+                      {
+                        "text": "Chip 2",
+                        "image": {
+                          "src": {
+                            "rawUrl": "https://example.com/images/logo.png"
+                          }
+                        },
+                        "link": "https://example.com"
+                      }
+                    ]
+                  }
+                ]
+              ]
+            },
            
             responseType: 'HANDLER_PROMPT',
             source: 'VIRTUAL_AGENT'
         }
     );
-        messages.push(
-            {
-                // text: {
-                //     text: [text],
-                //     redactedText: [text]
-                // },
-                payload:{
-                  "richContent": [
-                    [
-                      {
-                        "type": "chips",
-                        "options": [
-                          {
-                            "text": "Chip 1",
-                            "image": {
-                              "src": {
-                                "rawUrl": "https://example.com/images/logo.png"
-                              }
-                            },
-                            "link": "https://example.com"
-                          },
-                          {
-                            "text": "Chip 2",
-                            "image": {
-                              "src": {
-                                "rawUrl": "https://example.com/images/logo.png"
-                              }
-                            },
-                            "link": "https://example.com"
-                          }
-                        ]
-                      }
-                    ]
-                  ]
-                },
-                responseType: 'HANDLER_PROMPT',
-                source: 'VIRTUAL_AGENT'
-            }
-        );
+       
     // });
 
     let responseData = {

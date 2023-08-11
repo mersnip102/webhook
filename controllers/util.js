b6f49a6056766bbaa2bd93fc6605e37a7e364faf
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-08-11 17:19:53 +0700

    first commit

--- controllers/util.js
+++ controllers/util.js
@@ -9,41 +9,48 @@ const formatResponseForDialogflow = (texts, sessionInfo, targetFlow, targetPage)
                 text: [texts],
                 redactedText: [texts[0]]
             },
-            payload:{
-              "richContent": [
-                [
-                  {
-                    "type": "chips",
-                    "options": [
-                      {
-                        "text": "Chip 1",
-                        "image": {
-                          "src": {
-                            "rawUrl": "https://example.com/images/logo.png"
-                          }
-                        },
-                        "link": "https://example.com"
-                      },
-                      {
-                        "text": "Chip 2",
-                        "image": {
-                          "src": {
-                            "rawUrl": "https://example.com/images/logo.png"
-                          }
-                        },
-                        "link": "https://example.com"
-                      }
-                    ]
-                  }
-                ]
-              ]
-            },
+           
            
             responseType: 'HANDLER_PROMPT',
             source: 'VIRTUAL_AGENT'
         },
         
     );
+    // messages.push({
+    //   payload:{
+    //     "richContent": [
+    //       [
+    //         {
+    //           "type": "chips",
+    //           "options": [
+    //             {
+    //               "text": "Chip 1",
+    //               "image": {
+    //                 "src": {
+    //                   "rawUrl": "https://example.com/images/logo.png"
+    //                 }
+    //               },
+    //               "link": "https://example.com"
+    //             },
+    //             {
+    //               "text": "Chip 2",
+    //               "image": {
+    //                 "src": {
+    //                   "rawUrl": "https://example.com/images/logo.png"
+    //                 }
+    //               },
+    //               "link": "https://example.com"
+    //             }
+    //           ]
+    //         }
+    //       ]
+    //     ]
+    //   },
+    //   responseType: 'HANDLER_PROMPT',
+    //         source: 'VIRTUAL_AGENT'
+    // })
+
+    
         // messages.push(
         //     {
         //         // text: {

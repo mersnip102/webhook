3cd80a9a62c2c77b770359e28999d0bc5c876f9c
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-08-11 17:37:57 +0700

    first commit

--- controllers/util.js
+++ controllers/util.js
@@ -1,98 +1,48 @@
 const formatResponseForDialogflow = (texts, sessionInfo, targetFlow, targetPage) => {
 
-    let messages = []
-    
-    console.log("okeeeeeeeeeeeeeeeeeeeeeeeeeee", texts)
+    messages = []
 
-    // texts.forEach(text => {
-      messages.push(
-        {
-            text: {
-                text: texts,
-                redactedText: [texts[0]]
-            },
-           
-           
-            responseType: 'HANDLER_PROMPT',
-            source: 'VIRTUAL_AGENT'
-        },
-        
-    );
-    // messages.push({
-    //   payload:{
-    //     "richContent": [
-    //       [
-    //         {
-    //           "type": "chips",
-    //           "options": [
-    //             {
-    //               "text": "Chip 1",
-    //               "image": {
-    //                 "src": {
-    //                   "rawUrl": "https://example.com/images/logo.png"
-    //                 }
-    //               },
-    //               "link": "https://example.com"
-    //             },
-    //             {
-    //               "text": "Chip 2",
-    //               "image": {
-    //                 "src": {
-    //                   "rawUrl": "https://example.com/images/logo.png"
-    //                 }
-    //               },
-    //               "link": "https://example.com"
-    //             }
-    //           ]
-    //         }
-    //       ]
-    //     ]
-    //   },
-    //   responseType: 'HANDLER_PROMPT',
-    //         source: 'VIRTUAL_AGENT'
-    // })
-
-
-        // messages.push(
-        //     {
-        //         // text: {
-        //         //     text: [text],
-        //         //     redactedText: [text]
-        //         // },
-        //         payload:{
-        //           "richContent": [
-        //             [
-        //               {
-        //                 "type": "chips",
-        //                 "options": [
-        //                   {
-        //                     "text": "Chip 1",
-        //                     "image": {
-        //                       "src": {
-        //                         "rawUrl": "https://example.com/images/logo.png"
-        //                       }
-        //                     },
-        //                     "link": "https://example.com"
-        //                   },
-        //                   {
-        //                     "text": "Chip 2",
-        //                     "image": {
-        //                       "src": {
-        //                         "rawUrl": "https://example.com/images/logo.png"
-        //                       }
-        //                     },
-        //                     "link": "https://example.com"
-        //                   }
-        //                 ]
-        //               }
-        //             ]
-        //           ]
-        //         },
-        //         responseType: 'HANDLER_PROMPT',
-        //         source: 'VIRTUAL_AGENT'
-        //     }
-        // );
-    // });
+    texts.forEach(text => {
+        messages.push(
+            {
+                // text: {
+                //     text: [text],
+                //     redactedText: [text]
+                // },
+                payload: {
+                  "richContent": [
+                    [
+                      {
+                        "type": "chips",
+                        "options": [
+                          {
+                            "text": "Chip 1",
+                            "image": {
+                              "src": {
+                                "rawUrl": "https://example.com/images/logo.png"
+                              }
+                            },
+                            "link": "https://example.com"
+                          },
+                          {
+                            "text": "Chip 2",
+                            "image": {
+                              "src": {
+                                "rawUrl": "https://example.com/images/logo.png"
+                              }
+                            },
+                            "link": "https://example.com"
+                          }
+                        ]
+                      }
+                    ]
+                  ]
+                },
+                responseType: 'HANDLER_PROMPT',
+                source: 'VIRTUAL_AGENT'
+            }
+        );
+    });
 
     let responseData = {
         fulfillment_response: {
